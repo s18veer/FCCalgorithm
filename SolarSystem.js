b362bde854fd53d2a5c9edf4bdfaceff7a4f299f
Dharmveer Sharma <sdvsharma98@gmail.com>
2017-03-19 19:20:15 +0530

    Create SolarSystem.js

---
+++ SolarSystem.js
@@ -0,0 +1,53 @@
+var scene = new THREE.Scene();
+			var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 99999999 );
+
+			var renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+var oControls = new THREE.OrbitControls(camera, renderer.domElement);
+scene.add(new THREE.AmbientLight( 0xffffff, 0.4 ))
+      //Create elements here:
+function createCanvasMaterial(color, size) {
+	var matCanvas = document.createElement('canvas');
+  matCanvas.width = matCanvas.height = size;
+  var matContext = matCanvas.getContext('2d');
+  // create exture object from canvas.
+  var texture = new THREE.Texture(matCanvas);
+  // Draw a circle
+  var center = size / 2;
+  matContext.beginPath();
+  matContext.arc(center, center, size/2, 0, 2 * Math.PI, false);
+  matContext.closePath();
+  matContext.fillStyle = color;
+  matContext.fill();
+  // need to set needsUpdate
+  texture.needsUpdate = true;
+  // return a texture made from the canvas
+  return texture;
+}
+addNote()
+function deg(i) {
+  return i * Math.PI / 180
+}
+function RGB(r, g, b) {
+  function colorcheck(c) {
+  if (c > 255) {
+    c = 255
+  }
+  if (c < 0) {
+    c = 0
+  }
+    return c
+  }
+  r = colorcheck(r)
+  g = colorcheck(g)
+  b = colorcheck(b)
+  return 'rgb(' + r + ',' + g + ',' + b + ')'
+}
+function rgb2hex(rgb){
+ rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+ return (rgb && rgb.length === 4) ? "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+}
