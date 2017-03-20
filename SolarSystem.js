1c20d2f4459ac89c79834729f8662b40a78c4d73
Dharmveer Sharma <sdvsharma98@gmail.com>
2017-03-20 18:59:00 +0530

    Update

--- SolarSystem.js
+++ SolarSystem.js
@@ -51,3 +51,57 @@ function rgb2hex(rgb){
   ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
   ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
 }
+function rotateAround(point, center, angle) {
+  angle = (angle) * (Math.PI/180); // Convert to radians
+var rotatedX = Math.cos(angle) * (point.x - center.x) - 
+    Math.sin(angle) * (point.y-center.y) + 
+    center.x;
+var rotatedY = Math.sin(angle) * (point.x - center.x) + Math.cos(angle) * (point.y - center.y) + center.y;
+  return {x: rotatedX, y: rotatedY}
+}
+function rotateAround3d(point, center, angle) {
+  angle = (angle) * (Math.PI/180); // Convert to radians
+var rotatedX = Math.cos(angle) * (point.x - center.x) - 
+    Math.sin(angle) * (point.y-center.y) + 
+    center.x;
+var rotatedY = Math.sin(angle) * (point.x - center.x) + Math.cos(angle) * (point.y - center.y) + center.y;
+  return {x: rotatedX, y: rotatedY}
+}
+function rgb2color(r, g, b) {
+  return dec2hex(rgb2hex(RGB(r, g, b)))
+}
+function dec2hex(i) {
+    var result = "0x"
+   result = result + i.slice(1)
+  return result
+  }
+function randint(min, max) {
+  return Math.floor(Math.random() * max) + min
+}
+function randoffset(num, off) {
+  return randint(num - off, off * 2)
+}
+//three.js adding objects:
+var light = new THREE.PointLight('yellow', 3, 3000);
+light.position.set( 0, 0, 0 );
+scene.add(light);
+var geometry = new THREE.TetrahedronGeometry( 200, 5);
+var c = 0
+console.log(255 / geometry.faces.length)
+for ( var i = 0; i < geometry.faces.length; i ++ ) {
+
+    var face = geometry.faces[ i ];
+    color = rgb2color(255, randint(0, 150), 0)
+  c += 255 / geometry.faces.length
+  //console.log(c)
+    face.color.setHex( color );
+
+}
+var noise = 5
+for(var i=0; i<geometry.vertices.length; i++){
+      var v = geometry.vertices[i];
+      v.x += -noise/2 + Math.random()*noise;
+      v.y += -noise/2 + Math.random()*noise;
+      v.z += -noise/2 + Math.random()*noise;
+}
+var sun = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors } ));
