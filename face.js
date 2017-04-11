5bc5664f005083ced1533e6cbd0b6c37da91f959
iamdharmveer <sdvsharma98@gmail.com>
2017-04-11 14:13:44 +0000

    Face.js

--- face.js
+++ face.js
@@ -5,7 +5,7 @@ function sceneTwo() {
   var tl = new TimelineLite();
   tl.from(contour, 1.5, {
     scale: .9,
-    transformOrigin: "50% 50%",
+    transformOrigin: "60% 50%",
     fill: '#000000'
 
   }, 'face');
