3d35f711efac122f6a0b01f6107cf717cdb26660
iamdharmveer <sdvsharma98@gmail.com>
2017-04-19 22:49:39 +0530

    Final Change

--- wave7.js
+++ wave7.js
@@ -1,9 +1,40 @@
+var xspacing = 40;    // Distance between each horizontal location
+var w;                // Width of entire wave
+var theta = 0.0;      // Start angle at 0
+var amplitude = 85.0; // Height of wave
+var period = 289.0;   // How many pixels before the wave repeats
+var dx;               // Value for incrementing x
+var yvalues;  // Using an array to store height values for the wave
 
-var xspacing = 20;    // Distance between each horizontal locationvar w;                // Width of entire wavevar theta = 0.0;      // Start angle at 0var amplitude = 75.0; // Height of wavevar period = 300.0;   // How many pixels before the wave repeatsvar dx;               // Value for incrementing xvar yvalues;  // Using an array to store height values for the wave
+function setup() {
+  createCanvas(1300, 400);
+  w = width+19;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w/xspacing));
+  renderWave(7);
+}
+function draw() {
+  background(220);
+  calcWave();
+  renderWave();
+  }
 
-function setup() {   createCanvas(1600, 400);  w = width+16;  dx = (TWO_PI / period) * xspacing;  yvalues = new Array(floor(w/xspacing));  renderWave(7);}
-function draw() {    background(250);  calcWave();  renderWave();      }
-function calcWave() {  // Increment theta (try different values for   // 'angular velocity' here)  theta += 0.05;
-  // For every x value, calculate a y value with sine function  var x = theta;  for (var i = 0; i < yvalues.length; i++) {    yvalues[i] = sin(x)*amplitude;    x+=dx;  }}
-function renderWave(level) {  noStroke();  fill(0, 102, 153);
-  // A simple way to draw the wave with an ellipse at each location  for (var x = 0; x < yvalues.length; x++) {    ellipse(x*xspacing, height/2+yvalues[x], 16, 16);  }}
+function calcWave() {
+  // Increment theta (try different values for 'angular velocity' here)
+  theta += 0.05;
+// For every x value, calculate a y value with sine function
+  var x = theta;
+  for (var i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x)*amplitude;
+    x+=dx;
+  }
+}
+
+function renderWave(level) {
+  noStroke();
+  fill(0, 102, 153);
+// A simple way to draw the wave with an ellipse at each location
+  for (var x = 0; x < yvalues.length; x++) {
+    ellipse(x*xspacing, height/2+yvalues[x], 16, 16);
+  }
+}
