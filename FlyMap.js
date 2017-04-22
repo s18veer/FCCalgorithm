29855c8f760b9aff77159e15b1b62eb5d47f6806
Dharmveer Sharma <sdvsharma98@gmail.com>
2017-04-22 20:35:34 +0530

    Update

--- FlyMap.js
+++ FlyMap.js
@@ -1,6 +1,11 @@
-
-var numbers = [4, 9, 16, 25];
-
-function myFunction() {
-    x = document.getElementById("map")
-    x.innerHTML = numbers.map(Math.sqrt);
+ function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
