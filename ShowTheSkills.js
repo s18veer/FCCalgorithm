8ee7abed8ec083aec7310b37b016fe4fa52b5068
Dharmveer Sharma <sdvsharma98@gmail.com>
2017-04-25 22:07:18 +0530

    Update

--- ShowTheSkills.js
+++ ShowTheSkills.js
@@ -1,1 +1,26 @@
+var gage = {
+  init: function(){
+     
+    $('.chart span').css({"width" : "0"}).parent().each(function(i){
+      
+      $('p', this).html($(this).attr("data-label"));
+      
+      var timeout = parseInt(i) * 60 + 1100;
+       
+      $('span', this).delay(timeout).animate({"opacity" : "1"}, 0, function(){
+        
+        $(this).css({"width" : $(this).parent().attr("data-level") + "%"});
+      });
+    });
+  }
+}
+
+$(document).ready(function(){
+  
+  gage.init();
+    
+  setInterval(function() {
+      gage.init();
+  }, 5000);
+});
 
